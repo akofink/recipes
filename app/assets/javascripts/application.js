e46ad522f54c91473a49d2ea8aafe7ed641c54ce
Andrew Kofink <andrew.kofink@scimedsolutions.com>
2013-06-19 09:03:58 -0400

    Load assets in heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require_tree ../../../lib/assets
+//= require_tree ../../../app/public/assets
 //= require turbolinks
 //= require jquery
 //= require jquery_ujs
