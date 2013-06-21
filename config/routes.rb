b3bb579f27d857f24500695d415ecac1c0855cfa
Andrew Kofink <ajkofink@gmail.com>
2013-06-20 23:40:24 -0400

    Fix user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Recipes::Application.routes.draw do
-  get "users/create"
-  get "users/show"
-  get "users/update"
-  get "users/destroy"
-  get "users/index"
-  get "users/new"
-  get "users/edit"
+  resources :users
   resources :recipes
 
   match 'most_recent' => 'recipes#most_recent', via: :get
