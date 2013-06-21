e2ab755b16109d8cce932b1adc181c5b6802a723
Andrew Kofink <ajkofink@gmail.com>
2013-06-20 23:25:43 -0400

    Allow Users to authenticate with a password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  def self.login(email, password)
+    User.find_by(email: email).try(:authenticate, password)
+  end
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe User do
+
+  it 'should log in a user' do
+    User.create!(
+      email: 'a@b.c',
+      password: 'P@$$w0rD',
+      password_confirmation: 'P@$$w0rD'
+    )
+
+    User.login('a@b.c', 'P@$$w0rD').should be_true
+  end
+
+end
