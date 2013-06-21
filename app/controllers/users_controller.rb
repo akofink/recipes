c2c8ab2baba216d7c6b9de2696f326ffdbe197cb
Andrew Kofink <ajkofink@gmail.com>
2013-06-20 23:38:30 -0400

    Update the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,29 @@
 class UsersController < ApplicationController
   def create
+    User.save!(params)
   end
 
   def show
+    @user = User.find_by id: params[:id]
   end
 
   def update
+    @user = User.find_by id: params[:id]
   end
 
   def destroy
+    User.find_by(id: params[:id]).destroy
   end
 
   def index
+    @users = User.order :email
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find_by id: params[:id]
   end
 end
