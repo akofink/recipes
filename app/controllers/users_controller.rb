718e5dc0c8789cc323dd1dcb2be6a9378ad8e417
Andrew Kofink <ajkofink@gmail.com>
2013-06-20 23:37:57 -0400

    Add users controller and views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Recipes::Application.routes.draw do
+  get "users/create"
+  get "users/show"
+  get "users/update"
+  get "users/destroy"
+  get "users/index"
+  get "users/new"
+  get "users/edit"
   resources :recipes
 
   match 'most_recent' => 'recipes#most_recent', via: :get
