4298fc95de94c84cd7e904fbd89ea1dc80662429
Andrew Kofink <andrew.kofink@scimedsolutions.com>
2013-07-03 22:40:21 -0400

    Add tests to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ActiveRecord::Base
+  validates :title, presence: true
+  validates :body, presence: true
 end
---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Recipe do
+  it 'requires a title' do
+    @recipe = Recipe.new(
+      title: '',
+      body: 'test'
+    )
+
+    @recipe.should_not be_valid
+  end
+
+  it 'requires a body' do
+    @recipe = Recipe.new(
+      title: 'test',
+      body: ''
+    )
+
+    @recipe.should_not be_valid
+  end
+end
