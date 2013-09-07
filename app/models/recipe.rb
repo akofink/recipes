038b849cecd3064851d1ce52d8b08ccf0320e4b2
Andrew Kofink <ajkofink@gmail.com>
2013-09-07 10:47:17 -0400

    Add a comments model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,7 @@ class Recipe < ActiveRecord::Base
   validates :body, presence: true
 
   has_many :images, dependent: :destroy
+  has_many :comments, dependent: :destroy
   belongs_to :user
   accepts_nested_attributes_for :images, allow_destroy: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   has_many :images
   has_many :recipes
+  has_many :comments, dependent: :destroy
 
   validates :email,
     presence: true,
