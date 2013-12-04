44769aeefde336de2a38c9964a39259bd516375c
Andrew Kofink <ajkofink@gmail.com>
2013-12-04 10:39:44 -0500

    Change to use google storage instead of aws

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['aws_access_key_id'],
-    aws_secret_access_key: ENV['aws_secret_access_key']
+    provider: 'Google',
+    google_storage_access_key_id: ENV['google_storage_access_key_id'],
+    google_storage_secret_access_key: ENV['google_storage_secret_access_key']
   }
   config.fog_directory  = 'akrecipes'
 end
