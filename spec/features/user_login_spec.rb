777e97809a390f51b338d389a8f8654b859bc2d6
Andrew Kofink <ajkofink@gmail.com>
2013-12-26 10:10:35 -0500

    Add a user login integration spec

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+feature 'User logs in' do
+  scenario 'with valid email and password' do
+    User.create(email: 'akrecipes@mailinator.com', password: 'Passw0rd', password_confirmation: 'Passw0rd')
+    log_in email: 'akrecipes@mailinator.com', password: 'Passw0rd'
+    expect(page).to have_content('Login successful')
+  end
+
+  scenario 'with invalid email or password' do
+    log_in email: 'akrecipes@mailinator.com', password: 'password'
+    expect(page).to have_content('Login failed!')
+  end
+
+  def log_in(args = {})
+    visit '/'
+    click_link 'Login'
+    fill_in :email, with: args[:email]
+    fill_in :password, with: args[:password]
+    click_button 'Login'
+  end
+end
