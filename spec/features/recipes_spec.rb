aad779c029d87894fa20ceff5491c97a6a0ae979
Andrew Kofink <ajkofink@gmail.com>
2013-12-27 23:11:04 -0500

    Increase recipe integration tests

---
+++ spec/features/recipes_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+feature 'Recipe' do
+  scenario 'is shown to an unauthenticated user' do
+    r = Recipe.create(title: 'jfsowjro', body: 'body', user_id: 1)
+    visit "/recipes/#{r.id}"
+    expect(page).to have_content('jfsowjro')
+  end
+end
